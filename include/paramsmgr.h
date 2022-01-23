68958adaae845ce1132a21da3955af56f74228bb
Tim ONeil <tim@cipherloc.net>
2022-01-23 10:27:39 -0800

    More work on the params mgr

--- include/paramsmgr.h
+++ include/paramsmgr.h
@@ -5,31 +5,57 @@
 #include <jsoncpp/json/value.h>
 #include <jsoncpp/json/json.h>
 
-class ParamsMgr
+namespace slicer
 {
-	std::string _filepath;
-	Json::Reader _reader;
-	Json::Value _root;
-	Json::StyledStreamWriter _writer;
-
-public:
-	explicit ParamsMgr(const std::string& path) : _filepath(path)
+	struct STL2gcodeParams
 	{
+		float layer_height = 0.2f;
+		float nozzle_diameter = 0.25f;
+		float thread_thickness = 1.75f;
+
+		float top_bottom_thickness = 0.4f;
+		float filling_density = 0.15;
+		int nozzle_temperature = 205;
+
+		int table_temperature = 55;
+		int printing_speed = 35;
+		int filling_speed = 40;
+
+		int moving_speed = 90;
+		int printer_width = 215;
+		int printer_depth = 215;
+
+		int printer_height = 300;
+		float near_point = 0.00002f;
+		float near_distance = 0.03f;
+	};
+
+	class ParamsMgr
+	{
+		std::string _filepath;
+		Json::Reader _reader;
 		Json::Value _root;
-		if(!_reader.parse(_filepath, _root)) {
-			throw std::runtime_error(_reader.getFormattedErrorMessages());
-			return;
+		Json::StyledStreamWriter _writer;
+
+	public:
+		explicit ParamsMgr(const std::string& path) : _filepath(path)
+		{
+			Json::Value _root;
+			if(!_reader.parse(_filepath, _root)) {
+				throw std::runtime_error(_reader.getFormattedErrorMessages());
+				return;
+			}
 		}
-	}
 
-	void WriteParams();
+		void WriteParams();
 
-	// So we can have arbitraty return types w/one method
-	template<class T>
-	decltype(g(T())) GetParam()
-	{
-		return g(T());
-	}
-};
+		// So we can have arbitraty return types w/one method
+		template<class T>
+		decltype(g(T())) GetParam()
+		{
+			return g(T());
+		}
+	};
+}
 
 #endif // PARAMSMGR_H_
--- include/stl2gcode.h
+++ include/stl2gcode.h
@@ -17,29 +17,8 @@
 #include <filesystem>
 #include <paramsmgr.h>
 
-struct STL2gcodeParams
-{
-	float layer_height = 0.2f;
-	float nozzle_diameter = 0.25f;
-	float thread_thickness = 1.75f;
 
-	float top_bottom_thickness = 0.4f;
-	float filling_density = 0.15;
-	int nozzle_temperature = 205;
-
-	int table_temperature = 55;
-	int printing_speed = 35;
-	int filling_speed = 40;
-
-	int moving_speed = 90;
-	int printer_width = 215;
-	int printer_depth = 215;
-
-	int printer_height = 300;
-	float near_point = 0.00002f;
-	float near_distance = 0.03f;
-};
-
+// Might spin this out into its own TU like the params class
 class STLDivider
 {
 	std::vector<std::pair<char*, int>> slicecontainer;
@@ -66,7 +45,7 @@ class STL2gcode
 	std::set<int> planes;
 	std::string file;
 
-	STL2gcodeParams _params;
+	slicer::STL2gcodeParams _params;
 
 	void stl_binary(void);
 	void stl_ascii(void);
@@ -80,7 +59,7 @@ class STL2gcode
 	void processSTL(const std::string&);
 
 public:
-    explicit STL2gcode(const std::string&, const STL2gcodeParams&);
+    explicit STL2gcode(const std::string&, const slicer::STL2gcodeParams&);
     void convert(const std::string&);
 };
 
